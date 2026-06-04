a76bdcb6fabdc9d8054d75fa25bc0531c1707c5f
JessicaSB <santosbertoj@gmail.com>
2026-06-04 16:19:23 -0300

    Creating Players and Enemies Moving players and enemies

--- code/entityFactory.py
+++ code/entityFactory.py
@@ -1,9 +1,30 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import random
+from random import randint
+
+from code.Const import WIN_WIDTH, WIN_HEIGHT
+from code.background import Background
+from code.enemy import Enemy
+from code.player import Player
+
 
 class EntityFactory:
-    def __init__(self):
-        pass
 
-    def get_entity(self, entity_type):
-        pass
+    @staticmethod
+    def get_entity(entity_name: str, position=(0, 0)):
+        match entity_name:
+            case 'Level1Bg':
+                list_bg = []
+                for i in range(7):
+                    list_bg.append(Background(f"Level1Bg{i}", (0, 0)))
+                    list_bg.append(Background(f"Level1Bg{i}", (WIN_WIDTH, 0)))
+                return list_bg
+            case 'Player1':
+                return Player('Player1', position(10, WIN_HEIGHT // 2 - 30))
+            case 'Player2':
+                return Player('Player2', position(10, WIN_HEIGHT / 2 + 30))
+            case 'Enemy1':
+                return Enemy('Enemy1', (WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT - 40)))
+            case 'Enemy2':
+                return Enemy('Enemy2', (WIN_WIDTH + 10, random.randint(40, WIN_HEIGHT - 40)))
--- code/menu.py
+++ code/menu.py
@@ -1,17 +1,18 @@
-import pygame
+import pygame.image
 from pygame import Surface, Rect
 from pygame.font import Font
-from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE
+from code.Const import WIN_WIDTH, COLOR_ORANGE, MENU_OPTION, COLOR_WHITE, COLOR_YELLOW
 
 
 class Menu:
     def __init__(self, window):
         self.window = window
-        self.surf = pygame.image.load('./asset/PRE_ORIG_SIZE.png').convert_alpha()
+        self.surf = pygame.image.load('./asset/MenuBg.png').convert_alpha()
         self.rect = self.surf.get_rect(left=0, top=0)
 
     def run(self):
-        pygame.mixer.music.load('./asset/644413__vibritherabjit123__relaxing-dubstep-music.wav')
+        menu_option = 0
+        pygame.mixer.music.load('./asset/Menu.mp3')
         pygame.mixer.music.play(-1)
 
         while True:
@@ -28,20 +29,38 @@ class Menu:
                 text_center_pos=(WIN_WIDTH / 2, 120))
 
             for i in range(len(MENU_OPTION)):
-                self.menu_text(
-                text_size=20,
-                text=MENU_OPTION[i],
-                text_color=COLOR_WHITE,
-                text_center_pos=(WIN_WIDTH / 2, 200 + 25 * i))
-            pygame.display.flip()
+                if i == menu_option:
+                    self.menu_text(
+                        text_size=20,
+                        text=MENU_OPTION[i],
+                        text_color=COLOR_YELLOW,
+                        text_center_pos=(WIN_WIDTH / 2, 200 + 25 * i))
+                else:
+                    self.menu_text(
+                        text_size=20,
+                        text=MENU_OPTION[i],
+                        text_color=COLOR_WHITE,
+                        text_center_pos=(WIN_WIDTH / 2, 200 + 25 * i))
+                pygame.display.flip()
 
             # Check for all events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     quit()
-
-        pygame.quit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DOWN:
+                        if menu_option < len(MENU_OPTION) - 1:
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+                    if event.key == pygame.K_UP:
+                        if menu_option > 0:
+                            menu_option -= 1
+                        else:
+                            menu_option = len(MENU_OPTION) - 1
+                    if event.key == pygame.K_RETURN:  # Enter
+                        return MENU_OPTION[menu_option]
 
     def menu_text(self, text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
