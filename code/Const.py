502c34d51f7787bd94db9e9e750114bc4f9696ac
JessicaSB <santosbertoj@gmail.com>
2026-06-09 19:34:31 -0300

    SQLIte3 Implemantation for score using proxy design

--- code/Const.py
+++ code/Const.py
@@ -1,6 +1,7 @@
-from turtledemo.nim import COLOR
+
 
 import pygame
+from pygame.examples.grid import WINDOW_WIDTH
 
 # C
 C_ORANGE = (255, 128, 0)
@@ -12,6 +13,7 @@ C_CYAN = (0, 128, 128)
 
 # E
 EVENT_ENEMY =pygame.USEREVENT + 1
+EVENT_TIMEOUT = pygame.USEREVENT + 2
 ENTITY_SPEED = {
     'Level1Bg0': 0,
     'Level1Bg1': 1,
@@ -20,6 +22,11 @@ ENTITY_SPEED = {
     'Level1Bg4': 4,
     'Level1Bg5': 5,
     'Level1Bg6': 6,
+    'Level2Bg0': 0,
+    'Level2Bg1': 1,
+    'Level2Bg2': 2,
+    'Level2Bg3': 3,
+    'Level2Bg4': 4,
     'Player1': 3,
     'Player1Shot':1,
     'Player2': 3,
@@ -38,6 +45,11 @@ ENTITY_HEALTH = {
     'Level1Bg4': 999,
     'Level1Bg5': 999,
     'Level1Bg6': 999,
+    'Level2Bg0': 999,
+    'Level2Bg1': 999,
+    'Level2Bg2': 999,
+    'Level2Bg3': 999,
+    'Level2Bg4': 999,
     'Player1': 300,
     'Player1Shot': 1,
     'Player2': 300,
@@ -81,6 +93,11 @@ ENTITY_SCORE = {
     'Level1Bg4': 0,
     'Level1Bg5': 0,
     'Level1Bg6': 0,
+    'Level2Bg0': 0,
+    'Level2Bg1': 0,
+    'Level2Bg2': 0,
+    'Level2Bg3': 0,
+    'Level2Bg4': 0,
     'Player1': 0,
     'Player1Shot': 0,
     'Player2': 0,
@@ -114,13 +131,36 @@ PLAYER_KEY_LEFT = {'Player1' : pygame.K_LEFT,
                 'Player2' : pygame.K_a}
 PLAYER_KEY_RIGHT = {'Player1' : pygame.K_RIGHT,
                 'Player2' : pygame.K_d}
-PLAYER_KEY_SHOOT = {'Player1' : pygame.K_RCTRL,
+PLAYER_KEY_SHOOT = {'Player1' : pygame.K_RETURN,
                     'Player2' : pygame.K_LCTRL}
 
 
 #S
-SPAWN_TIME = 400000
+SPAWN_TIME = 4000
+
+
+
+#T
+TIMEOUT_STEP = 100 #100ms
+TIMEOUT_LEVEL = 20000 #20segundos
 
 #W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
+
+#S
+SCORE_POS = {'Title' : (WIN_WIDTH / 2, 50),
+             'EnterName' : (WIN_WIDTH / 2, 80),
+             'Label' : (WIN_WIDTH / 2, 90),
+             'Name' : (WIN_WIDTH / 2, 110),
+             0 : (WIN_WIDTH / 2, 110),
+             1 : (WIN_WIDTH / 2, 130),
+             2 : (WIN_WIDTH / 2, 150),
+             3 : (WIN_WIDTH / 2, 170),
+             4 : (WIN_WIDTH / 2, 190),
+             5 : (WIN_WIDTH / 2, 210),
+             6 : (WIN_WIDTH / 2, 230),
+             7 : (WIN_WIDTH / 2, 250),
+             8 : (WIN_WIDTH / 2, 270),
+             9 : (WIN_WIDTH / 2, 290),
+             }
